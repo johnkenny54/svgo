a0b383be53df43a790009bb98e022501d817094a
John Kenny <johnkenny54@yahoo.com>
2024-01-02 09:17:48 -0800

    Copy transformed SVGs to ../temp

--- optoxy.cmd
+++ optoxy.cmd
@@ -1,1 +1,1 @@
-node bin\svgo -i test\regression-fixtures\svgs\oxygen-icons-5.113.0\%1\%2.svg -o temp\%2.svg --multipass --config .\plugins\temp-config.js
+node bin\svgo -i test\regression-fixtures\svgs\oxygen-icons-5.113.0\%1\%2.svg -o ..\temp\%2.svg --multipass --config .\plugins\temp-config.js

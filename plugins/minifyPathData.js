8e42aee6ed1f07b8ff10b53f4ec73adc8f37ea49
John Kenny <johnkenny54@yahoo.com>
2024-08-24 17:22:29 -0700

    Only check for attribute selector at instantiation of plugin.

--- plugins/minifyPathData.js
+++ plugins/minifyPathData.js
@@ -1,6 +1,7 @@
 import { path2js, js2path } from './_path.js';
 import { pathElems } from './_collections.js';
 import { getDocData } from '../lib/docdata.js';
+import { visitSkip } from '../lib/xast.js';
 
 /**
  * @typedef {import('../lib/types.js').PathDataItem} PathDataItem
@@ -14,14 +15,16 @@ export const description = 'minifies path data';
  */
 export const fn = (root) => {
   const docData = getDocData(root);
+  const hasAttributeSelector = docData.styles.hasAttributeSelector('d');
+
   return {
     element: {
       enter: (node) => {
-        if (
-          pathElems.has(node.name) &&
-          node.attributes.d != null &&
-          !docData.styles.hasAttributeSelector('d')
-        ) {
+        if (hasAttributeSelector) {
+          // If there is an attribute selector on the "d" attribute, don't try to optimize.
+          return visitSkip;
+        }
+        if (pathElems.has(node.name) && node.attributes.d != null) {
           let data = path2js(node);
 
           if (data.length) {

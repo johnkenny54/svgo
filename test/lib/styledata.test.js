41f766618b6717483781aabe613a6f0688a69608
John Kenny <johnkenny54@yahoo.com>
2024-08-28 09:24:06 -0700

    Add check for script elements/attributes.

--- test/lib/styledata.test.js
+++ test/lib/styledata.test.js
@@ -2,5 +2,5 @@ import { generateData } from './testutils.js';
 
 test('import', () => {
   const data = generateData('./test/lib/docdata/style.import.1.svg');
-  expect(data.docData.styles).toBeUndefined();
+  expect(data.docData.styles).toBeNull();
 });
